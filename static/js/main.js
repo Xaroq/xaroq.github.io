fed189efc5629a601b256404cdb033dccb0a3842
zouzou135 <60619198+zouzou135@users.noreply.github.com>
2026-01-12 17:15:05 +0200

    Add files via upload
    new design, theme toggle, and a download button

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,20 @@
-console.log("Site loaded");
+const toggleBtn = document.getElementById("theme-toggle");
+const htmlElement = document.documentElement;
+
+toggleBtn.addEventListener("click", () => {
+  htmlElement.classList.toggle("dark");
+
+  const isDark = htmlElement.classList.contains("dark");
+  localStorage.setItem("theme", isDark ? "dark" : "light");
+  console.log("isDark", isDark);
+});
+
+if (
+  localStorage.getItem("theme") === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  htmlElement.classList.add("dark");
+} else {
+  htmlElement.classList.remove("dark");
+}
